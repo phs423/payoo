69f6057c3ae48ae4cd8630058e23843362d5d7ca
phs423 <phshawon22@gmail.com>
2026-02-21 16:34:16 +0600

    optimized cashout code

--- script/cashout.js
+++ script/cashout.js
@@ -1,30 +1,26 @@
 document.getElementById('cashout-btn').addEventListener('click', function () {
-    const cashOutNumber = document.getElementById('agent-number').value;
-
+    const cashOutNumber = getValueFromInput('agent-number');
     if (cashOutNumber.length != 11) {
         alert('Invalid Agent Number');
         return;
     }
 
-    const cashOutAmount = document.getElementById('amount-input').value;
+    const cashOutAmount = getValueFromInput('amount-input');
     if (cashOutAmount < 0) {
         alert('Invalid Amount');
         return;
     }
 
+
     const balance = document.getElementById('balance').innerText;
-
     const newBalance = Number(balance) - Number(cashOutAmount);
-
-
-
     if (newBalance < 0) {
         alert('Invalid Amount');
         return;
     }
 
-    const cashOutPin = document.getElementById('cashout-pin').value;
 
+    const cashOutPin = getValueFromInput('cashout-pin');
     if (cashOutPin === '1234') {
         alert('Cash out successful');
         document.getElementById('balance').innerText = newBalance;
@@ -35,4 +31,4 @@ document.getElementById('cashout-btn').addEventListener('click', function () {
         return;
     }
 
-}) 
+})
